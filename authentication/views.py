57ce010685a82c163b17e3265f41d448be340d0f
Latisha Johnson <latishaj966@gmail.com>
2021-03-01 15:08:19 -0800

    able to log in and register and have loginrequired for homepage

--- authentication/views.py
+++ authentication/views.py
@@ -2,11 +2,40 @@ from django.shortcuts import render, HttpResponseRedirect, reverse
 from twitteruser.models import TwitterUser
 from authentication.forms import SignUpForm, LoginForm
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            user = authenticate(request, username=data['username'], password=data['password'])
+            if user:
+                login(request, user)
+                return HttpResponseRedirect(request.GET.get('next', reverse('home_view')))
+
     form = LoginForm()
     return render(request, 'generic.html', {'form': form})
 
 def signup_view(request):
+    context = {}
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            TwitterUser.objects.create_user(
+                first_name = data['first_name'],
+                last_name = data['last_name'],
+                username = data['username'],
+                email = data['email'],
+                password = data['password'],
+            )
+            return HttpResponseRedirect(reverse('home_view'))
     form = SignUpForm()
-    return render(request, 'generic.html', {'form': form})
+    context.update({'form': form})
+    return render(request, 'generic.html', context)
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('login'))
